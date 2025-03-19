8bf2ef44e553cc0706e40af8621edb732787a833
FedericoBellezza <federico.bellezza2001@gmail.com>
2025-03-19 15:53:17 +0100

    add filter by name and Id

--- src/main/java/org/lessons/java/spring_la_mia_pizzeria_crud/controller/PizzaController.java
+++ src/main/java/org/lessons/java/spring_la_mia_pizzeria_crud/controller/PizzaController.java
@@ -1,8 +1,6 @@
 package org.lessons.java.spring_la_mia_pizzeria_crud.controller;
 
 import java.util.List;
-import java.util.Optional;
-
 import org.lessons.java.spring_la_mia_pizzeria_crud.model.Pizza;
 import org.lessons.java.spring_la_mia_pizzeria_crud.repository.PizzaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,6 +9,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequestMapping("/pizzas")
@@ -21,26 +20,24 @@ public class PizzaController {
 
     @GetMapping
     public String index(Model model) {
-        // prendo tutte le pizza dalle repository
         List<Pizza> pizzas = repository.findAll();
-        // passo le pizze con model
         model.addAttribute("pizzas", pizzas);
         return "pizzas/index";
     }
 
     @GetMapping("/{id}")
     public String show(Model model, @PathVariable Integer id) {
-        // prendo tutte le pizza dalle repository
-        List<Pizza> pizzas = repository.findAll();
-        Pizza foundPizza = null;
-
-        for (Pizza pizza : pizzas) {
-            if (pizza.getId().equals(id)) {
-                foundPizza = pizza;
-            }
-        }
-        // passo la pizza con model
-        model.addAttribute("pizza", foundPizza);
+        Pizza pizza = repository.findById(id).get();
+        model.addAttribute("pizza", pizza);
         return "pizzas/show";
     }
+
+    @GetMapping("/search-by-name")
+    public String searchByName(@RequestParam(name = "name") String name, Model model) {
+
+        List<Pizza> pizzas = repository.findByNameContaining(name);
+        model.addAttribute("pizzas", pizzas);
+        return "pizzas/index";
+    }
+
 }
